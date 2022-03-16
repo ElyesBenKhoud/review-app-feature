39d60b76896312b29cfc5c3299e683664f88c297
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2022-03-16 11:01:29 +0200

    mapping through the feedback list and display it

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
+import { useState } from "react";
 import "./App.css";
-import FeedbackItem from "./components/FeedbackItem";
 import Header from "./components/Header.jsx";
-
+import FeedbackData from "./data/FeedbackData";
+import FeedbackList from "./components/FeedbackList";
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
   return (
     <>
       <Header />
       <div className="container">
-        <FeedbackItem />
+        <FeedbackList feedback={feedback} />
       </div>
     </>
   );
