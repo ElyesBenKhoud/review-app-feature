ad10f5578a5c8d79a9e9752a15846e3cabee5a55
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2022-03-21 15:54:04 +0200

    update functionnality works

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -34,14 +34,21 @@ export const FeedbackProvider = ({ children }) => {
     });
   };
 
+  //update feedback lfunction
+  const updateFeedback = (id, updItem) => {
+    setFeedback(
+      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+    );
+  };
   return (
     <FeedbackContext.Provider
       value={{
         feedback,
+        feedbackEdit,
         deleteFeedback,
         addFeedback,
         editFeedback,
-        feedbackEdit,
+        updateFeedback,
       }}
     >
       {children}
