7db5d879bdfdfe2bbc30473955b4684572d1f1f8
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2022-03-21 15:22:38 +0200

    updating review button and link to context api done

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,18 +1,21 @@
 import Card from "./shared/Card";
 import propTypes from "prop-types";
-import { FaTimes } from "react-icons/fa";
+import { FaTimes, FaEdit } from "react-icons/fa";
 import React, { useContext } from "react";
 import FeedbackContext from "../context/FeedbackContext";
 
 const FeedbackItem = ({ item }) => {
   //calling delete function from context api
-  const { deleteFeedback } = useContext(FeedbackContext);
+  const { deleteFeedback, editFeedback } = useContext(FeedbackContext);
 
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
       <button className="close" onClick={() => deleteFeedback(item.id)}>
         <FaTimes color="purple" />
+      </button>
+      <button className="edit" onClick={() => editFeedback(item)}>
+        <FaEdit color="purple" />
       </button>
       <div className="text-display">{item.text}</div>
     </Card>
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -5,19 +5,33 @@ const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
   const [feedback, setFeedback] = useState([
-    { id: 1, text: "this item is from context API", rating: 10 },
+    { id: 1, text: "this item is review1 from context API", rating: 10 },
+    { id: 2, text: "this item is review2 from context API", rating: 5 },
+    { id: 3, text: "this item is review3 from context API", rating: 4.5 },
   ]);
 
+  const [feedbackEdit, setfeedbackEdit] = useState({
+    item: {},
+    edit: false,
+  });
+  //deleting feedback
   const deleteFeedback = (id) => {
     if (window.confirm("Are you sure you want to delete")) {
       //delete the item with the id clicked from the feedback array
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
-
+  //adding feedback
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4();
     setFeedback([newFeedback, ...feedback]);
+  };
+  //set the item tp be updated
+  const editFeedback = (item) => {
+    setfeedbackEdit({
+      item,
+      edit: true,
+    });
   };
 
   return (
@@ -26,6 +40,7 @@ export const FeedbackProvider = ({ children }) => {
         feedback,
         deleteFeedback,
         addFeedback,
+        editFeedback,
       }}
     >
       {children}
